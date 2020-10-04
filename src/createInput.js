a7b02807c556fc4decc9a8a05e4a7fc5bc3537e0
Herix <shazamwabamidiot@gmail.com>
2020-10-04 18:20:48 -0500

    v1.0.4

--- src/createInput.js
+++ src/createInput.js
@@ -3,7 +3,15 @@
  module.exports.startInput = async function (message) {
     // Time Limit
     let timeLimit = 30000;
-    setTime(number); { if(isNaN(number)) timeLimit = 30000; else timeLimit = number; }
+    // this.setTime = (number); if(isNaN(number)) timeLimit = 30000; else timeLimit = number; }
+    this.setTime = function(number){
+       if(isNaN(number)) {
+          timeLimit = 30000;
+       }
+       else {
+          timeLimit = number;
+       }
+    }
 
     const inputCollector = message.channel.createMessageCollector(m => (m.author.id === message.author.id), { max: 1, time: 15000 });
     inputCollector.on('collect', m => { return m; });
