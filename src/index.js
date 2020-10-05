f6096eec0db860fa771373f652c641cffd2b3bc2
Herix <shazamwabamidiot@gmail.com>
2020-10-04 19:15:22 -0500

    v1.0.6

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 const Discord = require('discord.js');
 
-// Input
-const { startInput } = require('./createInput');
-module.exports.createInput = async function(message) { return startInput(member, message) }
+module.exports.createInput = function(message) {
+    this.time = 30000;
+
+    class Time {
+        constructor(message) {
+             this.message = message
+        }
+        setTime(time) {
+               this.time = time
+        }
+    }
+     
+ 
+     const inputCollector = this.message.channel.createMessageCollector(m => (m.author.id === this.message.author.id), { max: 1, time: 30000 });
+     inputCollector.on('collect', m => { return console.log(m); });
+     setTimeout(() => {
+        return this.message.reply('too late!');
+     }, 30000);
+}
